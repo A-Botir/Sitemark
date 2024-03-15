bfeca91e2cacb312b2c0d17b4ba4bd330c7af7a7
A_Botir <shirokirutsuga>
2024-03-15 07:42:46 +0500

    image section

--- src/components/AuraImage.jsx
+++ src/components/AuraImage.jsx
@@ -2,9 +2,48 @@ import React from "react";
 
 const AuraImage = () => {
   return (
-    <div className="shadow-img mt-24 h-[700px] w-full rounded-[10px] outline outline-[#bfccd980]">
-      <div className="h-full w-full bg-[url('https://mui.com/static/images/templates/templates-images/hero-light.png')] bg-contain bg-no-repeat"></div>
-    </div>
+    <section className="mt-40">
+      <div className="shadow-img h-[700px] w-full rounded-[10px] outline outline-[#bfccd980]">
+        <div className="mb-24 h-full w-full bg-[url('https://mui.com/static/images/templates/templates-images/hero-light.png')] bg-contain bg-no-repeat"></div>
+        <div className="py-8">
+          <p className="mb-1 text-center text-[16px] font-medium text-[#4C5967]">
+            Trusted by the best companies
+          </p>
+          <div className="m-auto grid w-[75%] grid-cols-6 items-center justify-center gap-16">
+            <img
+              src="https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628889c3bdf1129952dc_Sydney-black.svg"
+              alt="syd"
+              className="opacity-60"
+            />
+            <img
+              src="https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d4d8b829a89976a419c_Bern-black.svg"
+              alt="bern"
+              className="opacity-60"
+            />
+            <img
+              src="https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f467502f091ccb929529d_Montreal-black.svg"
+              alt="montreal"
+              className="opacity-60"
+            />
+            <img
+              src="https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e911fa22f2203d7514c_TerraDark.svg"
+              alt="terra"
+              className="opacity-60"
+            />
+            <img
+              src="https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a0990f3717787fd49245_colorado-black.svg"
+              alt="colorado"
+              className="opacity-60"
+            />
+            <img
+              src="https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5ca4e548b0deb1041c33_Ankara-black.svg"
+              alt="ankara"
+              className="opacity-60"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
